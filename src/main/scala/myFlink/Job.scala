bf1e7b9b2415033e5f7050de209530d6fa7791c7
Dmitry <dmitry@dmitrypukhov.pro>
2017-02-14 20:48:01 +0300

    Graph added

--- src/main/scala/myFlink/Job.scala
+++ src/main/scala/myFlink/Job.scala
@@ -1,23 +1,5 @@
 package myFlink
 
-/**
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 import org.apache.flink.api.scala._
 
 /**
@@ -39,6 +21,10 @@ object Job {
   def main(args: Array[String]) {
     // set up the execution environment
     val env = ExecutionEnvironment.getExecutionEnvironment
+
+    val text = env.fromElements("To be, or not to be,--that is the question:--",
+      "Whether 'tis nobler in the mind to suffer", "The slings and arrows of outrageous fortune",
+      "Or to take arms against a sea of troubles,")
 
     /**
      * Here, you can start creating your execution plan for Flink.
